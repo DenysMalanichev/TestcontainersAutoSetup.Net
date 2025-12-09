6f229dcce0d4d5dd6bc78f6741b582d8716fc3c5
Denys Malanichev <deniskirillov228@gmail.com>
2025-12-10 10:21:41 +1100

    add a generic container builder test

--- Testcontainers.AutoSetup.Tests/IntegrationTests/ContainerBuilderExtensionsTests.cs
+++ Testcontainers.AutoSetup.Tests/IntegrationTests/ContainerBuilderExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿using DotNet.Testcontainers.Containers;
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Testcontainers.AutoSetup.Core.Abstractions;
@@ -40,6 +41,46 @@ public class ContainerBuilderExtensionsTests
             .WithLabel("reuse-id", "MsSQL-testcontainer-reuse-hash")
             .WithDbSeeder(
                 seederMock.Object, (c) => c.GetConnectionString())
+            .Build();
+        await container.StartAsync();
+
+        // Assert
+        Assert.Single(seederMock.Invocations);
+        Assert.NotNull(createdContainer);
+        Assert.Equal(TestcontainersStates.Running, createdContainer.State);
+    }
+
+    [Fact]
+    public async Task ContainerBuilderExtensions_WithDbSeeder_HooksInsideTheGenericContainer()
+    {
+        // Arrange 
+        const int systemPort = 23724;
+        IContainer createdContainer = null!;
+        var seederMock = new Mock<IDbSeeder>();
+        seederMock.Setup(
+            (seeder) =>
+                seeder.SeedAsync(
+                    It.IsAny<IContainer>(),
+                    It.IsAny<string>(),
+                    It.IsAny<CancellationToken>())
+        ).Callback((IContainer container, string _, CancellationToken _) => createdContainer = container);
+
+        // Act
+        var builder = new ContainerBuilder();
+        if(dockerEndpoint is not null)
+        {
+            builder = builder.WithDockerEndpoint(dockerEndpoint);
+        }
+        var container = builder
+            .WithName("GenericMsSQL-testcontainer")
+            .WithImage("mcr.microsoft.com/mssql/server:2025-latest")
+            .WithPortBinding(1433, systemPort)
+            .WithEnvironment("ACCEPT_EULA", "Y")
+            .WithEnvironment("SA_PASSWORD", "YourStrongPassword123!")
+            .WithReuse(reuse: !DockerAddressHelper.IsCiRun())
+            .WithDbSeeder(
+                seederMock.Object,
+                _ => $"Server=localhost,{systemPort};Database=master;User ID=sa;Password=YourStrongPassword123!;Encrypt=False;")
             .Build();
         await container.StartAsync();
 
